2c4d3514018258ea0da50d4794f37a192383d2ef
Yurie <cutecupcakeandcute@gmail.com>
2020-06-24 03:14:18 +0900

    add helper function to calculate tour length

--- common.py
+++ common.py
@@ -29,3 +29,18 @@ def create_adjacent_matrix(cities):
             dist[i][j] = dist[j][i] = distance(cities[i], cities[j])
     
     return dist
+
+
+def calculate_tour_length(tour_arr, dist_matrix):
+    N = len(tour_arr)
+    tours_length = list()
+  
+    for i in range(N):
+        total_tour_length = 0
+        for j in range(len(tour_arr[i]) - 1):
+            source = tour_arr[i][j]
+            destination = tour_arr[i][j + 1]
+            total_tour_length += dist_matrix[source][destination]
+        tours_length.append(total_tour_length)
+    
+    return tours_length
