b28f7f60cac2a1d77100c55682bbbdd5a1799fdd
Yurie <cutecupcakeandcute@gmail.com>
2020-06-23 14:34:37 +0900

    add helper function to create adjacent matrix for distance

--- common.py
+++ common.py
@@ -1,3 +1,5 @@
+import math
+
 def read_input(filename):
     with open(filename) as f:
         cities = []
@@ -13,3 +15,17 @@ def format_solution(solution):
 
 def print_solution(solution):
     print(format_solution(solution))
+
+
+def distance(city1, city2):
+    return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
+
+
+def create_adjacent_matrix(cities):
+    N = len(cities)
+    dist = [[float('inf')] * N for i in range(N)]
+    for i in range(N):
+        for j in range(N):
+            dist[i][j] = dist[j][i] = distance(cities[i], cities[j])
+    
+    return dist
